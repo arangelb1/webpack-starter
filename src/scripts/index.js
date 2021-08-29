4e342745be64ba49aa3691d29f73cf9e73fc74d7
Antonio Rangel <arangelb11@gmail.com>
2021-08-29 14:47:37 -0400

    Second commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,13 +10,64 @@
 // const carId = 100;
 
 // let carID = 42;
-
 console.log('Hello World!!!');
 
-let carId;
-
+let carId = 100;
 console.log(carId);
-
 carId = 50;
-
 console.log(carId);
+function sendCars(day, ...allCarIds) {
+    allCarIds.forEach( id => console.log(id));
+}
+sendCars('Monday', 100, 200, 555);
+let carIds = [1, 2, 5];
+
+// let [car1, car2, car3] = carIds;
+// console.log(car1, car2, car3);
+
+let car1, remainingCars;
+[car1, ... remainingCars] = carIds;
+console.log(car1, remainingCars);
+
+let car = { id: 5000, style: 'convertible'};
+let id, style;
+({ id, style } = car);
+console.log(id, style);
+
+function startCars(car1, car2, car3, ...rest) {
+    console.log(car1, rest);
+}
+
+let carCodes = [100, 300, 500, 'abc', 40, 20];
+
+startCars(...carCodes);
+
+console.log(typeof(1));
+console.log(typeof(true));
+console.log(typeof('Hello'));
+// console.log(typeof( function() {} );
+console.log(typeof({}));
+console.log(typeof(null));
+console.log(typeof(undefined));
+// console.log(typeof(NaN);
+
+console.log(carIds.toString());
+console.log(Number.parseInt('55'));
+console.log(Number.parseFloat('55.88ABC'));
+console.log(Number.parseFloat('Abc55.88'));
+
+let i=0;
+for (; i<5; i++) {
+    // console.log(i);
+    if (i === 3) {
+        break;
+    }
+}
+console.log(i);
+
+for (let i=0; i<4; i++) {
+     if (i === 2) {
+        continue;
+    }
+    console.log(i);
+}
